3d8d84ce3701112ce90799f853c23c7e9a5c32e2
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-06-15 19:34:15 -0300

    darkmode+buttons+extra

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import {
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider
+} from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 import Button from "@material-ui/core/Button";
+import RefreshIcon from "@material-ui/icons/Refresh";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Titulo from "./titulo";
 import Btn from "./btn";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     "& > *": {
       margin: theme.spacing(1)
     }
+  },
+  margin: {
+    margin: theme.spacing(1)
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center"
   }
 }));
+
 export default function ContainedButtons() {
   const classes = useStyles();
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? "dark" : "light"
+        }
+      }),
+    [prefersDarkMode]
+  );
+  function refreshPage() {
+    window.location.reload(false);
+  }
   var lis = [];
   //var abtn = [esp[0], esp[1], esp[2], ext[0], ext[1], ext[2]];
 
@@ -46,12 +79,24 @@ export default function ContainedButtons() {
   }
 
   return (
-    <React.Fragment>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Container>
-        <Titulo />
-        <div>{lis}</div>
-      </Container>
-    </React.Fragment>
+      <React.Fragment>
+        <CssBaseline />
+        <Container>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <Titulo />
+            </Paper>
+          </Grid>
+          <div className={classes.root}>
+            <Button variant="outlined" color="secondary" onClick={refreshPage}>
+              <RefreshIcon />
+            </Button>
+          </div>
+          <div>{lis}</div>
+        </Container>
+      </React.Fragment>
+    </ThemeProvider>
   );
 }
--- src/titulo.js
+++ src/titulo.js
@@ -5,8 +5,8 @@ import Typography from "@material-ui/core/Typography";
 //componente a exportar
 const Titulo = () => {
   return (
-    <Typography variant="h1" component="h2">
-      Red Card
+    <Typography variant="h3" component="h4">
+      Red Card FUT
     </Typography>
   );
 };
