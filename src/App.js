80ea56b14c6c99bb8d7ecd0ae68186827ed9494e
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-06-17 20:02:09 -0300

    20hs

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ export default function ContainedButtons() {
     );
     /*SWITCH*/
     switch (i) {
-      case 3 /*btn1*/:
+      case "3" /*btn1*/:
         lis.push(
           <div className={classes.root}>
             <Btn Valor={conmebol[0]} />
