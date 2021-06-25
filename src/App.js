644ce88b09d52a14b2c744efa1c07c2385f26540
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-06-24 23:18:21 -0300

    hthtt5erujerikeiretre

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ export default function ContainedButtons() {
           </div>
         );
         break;
-      case 1:
+      case "1":
         lis.push(
           <div className={classes.root}>
             <Btn Valor={conmebol[0]} />
