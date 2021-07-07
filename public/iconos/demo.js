528f195897bc5d2f68be666ded64fae4ed46b821
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-07-07 02:30:36 -0300

    Update demo.js

--- public/iconos/demo.js
+++ public/iconos/demo.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import LinearProgress from "@material-ui/core/LinearProgress";
-import Typography from "@material-ui/core/Typography";
+import React from "../react";
+import { makeStyles } from "../@material-ui/core/styles";
+import LinearProgress from "../@material-ui/core/LinearProgress";
+import Typography from "../@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
