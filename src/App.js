b0f6edbf32884f0c81856b3f69e4f59829ebed45
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-08-08 02:36:57 -0300

    0110101010101010

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ export default function ContainedButtons() {
           </div>
         );
         break;
-      case 0:
+      case 1120:
         lis.push(
           <div className={classes.root}>
             <Btn Valor={esp[0]} />
