0244ff8d9f775ab99749ac3377e4ca84f5453b58
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-08-07 23:01:30 -0300

    100110101010

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ export default function ContainedButtons() {
     );
     /*SWITCH*/
     switch (i) {
-      case 7:
+      case 70:
         lis.push(
           <div className={classes.root}>
             <Btn Valor={arg[0]} />
