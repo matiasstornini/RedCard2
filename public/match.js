286382b5c6ca280825cf11294901f20a78cb2da8
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-08-17 02:17:01 -0300

    010101010110001

--- public/match.js
+++ public/match.js
@@ -7,7 +7,7 @@ let Title = [
   "Premier League (Inglaterra)",
   "Premier League (Inglaterra)",*/
 
-  "LaLiga (España)",
+  /*"LaLiga (España)",
   "LaLiga (España)",
   /*"LaLiga (España)",*/
   /*"LaLiga (España)",*/
@@ -21,39 +21,40 @@ let Title = [
   "Bundesliga (Alemania)",
   "Bundesliga (Alemania)",
   "Bundesliga (Alemania)",*/
+  "SUPERCOPA ALEMANIA",
 
   /*"Copa Argentina","Copa Argentina",/*"Copa Argentina",*/
+  /*"Liga Profesional (Argentina)",
   "Liga Profesional (Argentina)",
   "Liga Profesional (Argentina)",
-  "Liga Profesional (Argentina)",
-  "Liga Profesional (Argentina)",
+  "Liga Profesional (Argentina)",*/
 
+  /*"Liga BetPlay (Colombia)",
   "Liga BetPlay (Colombia)",
-  "Liga BetPlay (Colombia)",
-  "Liga BetPlay (Colombia)",
+  "Liga BetPlay (Colombia)",*/
+
+  "Conmebol Libertadores",
+  "Conmebol Sudamericana",
 
   "ERROR"
 ];
 let Match = [
-  " Villarreal - Granada ",
-  " Elche - Athletic Club ",
-  " Talleres Córdoba - San Lorenzo ",
-  " Defensa y Justicia - Sarmiento ",
-  " Lanús - Godoy Cruz "
+  " Borussia Dortmund - Bayern München ",
+  " Palmeiras - São Paulo ",
+  " RB Bragantino - Rosario Central "
 ];
 
 let Est = [
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4045999/villarreal-vs-granada/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046000/elche-vs-athletic-club/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043852/talleres-cordoba-vs-san-lorenzo/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043845/defensa-y-justicia-vs-sarmiento/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043841/lanus-vs-godoy-cruz/ "
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4014085/borussia-dortmund-vs-bayern-m-nchen/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4075514/palmeiras-vs-sao-paulo/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4014085/borussia-dortmund-vs-bayern-m-nchen/ "
 ];
 
 let Enl = [
   "",
   "",
 
+  "",
   "",
   "",
   "",
@@ -87,6 +88,6 @@ let Enl = [
   "error"
 ];
 
-let HOY = "16/08/2021";
+let HOY = "17/08/2021";
 
 //console.log<b>(Est)</b></b>;
