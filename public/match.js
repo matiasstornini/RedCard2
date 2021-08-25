ac700f92b40abbc748ee012d4c3a39ba30fd8b75
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-08-25 00:49:20 -0300

    1011010101010111110110

--- public/match.js
+++ public/match.js
@@ -31,6 +31,7 @@ let Title = [
   "Liga Profesional (Argentina)",
   "Liga Profesional (Argentina)",
   "Liga Profesional (Argentina)",
+  "Liga Profesional (Argentina)",
   /*"Liga Profesional (Argentina)",*/
 
   "Liga BetPlay (Colombia)",
@@ -46,23 +47,27 @@ let Title = [
   "ERROR"
 ];
 let Match = [
-  " Patronato - Banfield ",
-  " Colón - Sarmiento ",
-  " Atlético Tucumán - Independiente ",
-  " Racing Club - Central Córdoba SdE "
+  " Rosario Central - Arsenal ",
+  " Defensa y Justicia - Newell's Old Boys ",
+  " Lanús - Gimnasia La Plata ",
+  " Huracán - Vélez Sarsfield ",
+  " Platense - Boca Juniors "
 ];
 
 let Est = [
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043876/patronato-vs-banfield/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043870/colon-vs-sarmiento/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043871/atletico-tucuman-vs-independiente/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043874/racing-club-vs-central-cordoba-sde/ "
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043873/rosario-central-vs-arsenal/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043872/defensa-y-justicia-vs-newells-old-boys/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043868/lanus-vs-gimnasia-la-plata/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043867/huracan-vs-velez-sarsfield/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043875/platense-vs-boca-juniors/ "
 ];
 
 let Enl = [
   "",
+
   "",
   "",
+
   "",
   "",
   "",
@@ -86,6 +91,6 @@ let Enl = [
   "error"
 ];
 
-let HOY = "24/08/2021";
+let HOY = "23/08/2021";
 
 //console.log<b>(Est)</b></b>;
