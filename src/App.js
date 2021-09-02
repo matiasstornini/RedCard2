09d90f421ba329567d651bad0842edff9f9999d1
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-09-02 20:04:54 -0300

    1010100101011110

--- src/App.js
+++ src/App.js
@@ -66,13 +66,11 @@ export default function ContainedButtons() {
     "",
     "",
     "",
+    "",
+    "",
     <div>
       {" "}
-      <Btn Valor={ext[0]} />{" "}
-    </div>,
-    <div>
-      {" "}
-      <Btn Valor={ext[1]} />{" "}
+      <Btn Valor={arg[0]} /> <Btn Valor={arg[1]} /> <Btn Valor={arg[2]} />{" "}
     </div>,
     "",
     "",
