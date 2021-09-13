e9939a8dd20bcddd7a9ce60e1e14a049c2f46373
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-09-13 02:07:06 -0300

    100110010101

--- public/match.js
+++ public/match.js
@@ -9,7 +9,7 @@ let Title = [
   "Clasificatorio para la Copa del Mundo",
   "Clasificatorio para la Copa del Mundo",/**/
 
-  "Premier League (Inglaterra)",
+  /*"Premier League (Inglaterra)",
   /*"Premier League (Inglaterra)",
   "Premier League (Inglaterra)",
   "Premier League (Inglaterra)",
@@ -21,14 +21,14 @@ let Title = [
 
   "LaLiga (España)",
   "LaLiga (España)",
-  "LaLiga (España)",
+  /*"LaLiga (España)",
   "LaLiga (España)",
   /**/
 
   /*"Ligue 1 (Francia)",
   /**/
 
-  "Serie A (Italia)",
+  /*"Serie A (Italia)",
   "Serie A (Italia)",
   "Serie A (Italia)",
   "Serie A (Italia)",
@@ -47,11 +47,11 @@ let Title = [
   /**/
 
   /*"Copa Argentina","Copa Argentina",/*"Copa Argentina",*/
-  /*"Liga Profesional (Argentina)",
   "Liga Profesional (Argentina)",
-  /*"Liga Profesional (Argentina)",
   "Liga Profesional (Argentina)",
-  /*"Liga Profesional (Argentina)",
+  "Liga Profesional (Argentina)",
+  "Liga Profesional (Argentina)",
+  "Liga Profesional (Argentina)",
   /*"Liga Profesional (Argentina)",*/
 
   "Liga BetPlay (Colombia)",
@@ -68,43 +68,23 @@ let Title = [
   "ERROR"
 ];
 let Match = [
-  " Leeds United - Liverpool ",
-  " Espanyol - Atlético Madrid ",
-  " Osasuna - Valencia ",
-  " Cádiz - Real Sociedad ",
-  " Real Madrid - Celta de Vigo ",
-  " Sampdoria - Internazionale ",
-  " Cagliari - Genoa ",
-  " Spezia - Udinese ",
-  " Torino - Salernitana ",
-  " Milan - Lazio ",
-  " Roma - Sassuolo ",
-  " Atlético Bucaramanga - Millonarios ",
-  " Deportivo Pereira - Junior ",
-  " Atlético Nacional - La Equidad "
+  " Getafe - Elche ",
+  " Granada - Real Betis ",
+  " Aldosivi - Godoy Cruz ",
+  " Sarmiento - Huracán ",
+  " Independiente - Lanús ",
+  " San Lorenzo - Racing Club ",
+  " Gimnasia La Plata - Vélez Sarsfield "
 ];
 
 let Est = [
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4027053/leeds-united-vs-liverpool/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046025/espanyol-vs-atletico-madrid/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046027/osasuna-vs-valencia/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046023/cadiz-vs-real-sociedad/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046028/real-madrid-vs-celta-de-vigo/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064558/sampdoria-vs-internazionale/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064553/cagliari-vs-genoa/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064559/spezia-vs-udinese/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064560/torino-vs-salernitana/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064555/milan-vs-lazio/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064557/roma-vs-sassuolo/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064558/sampdoria-vs-internazionale/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064553/cagliari-vs-genoa/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064559/spezia-vs-udinese/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064560/torino-vs-salernitana/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064555/milan-vs-lazio/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064557/roma-vs-sassuolo/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4060773/atletico-bucaramanga-vs-millonarios/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4060779/deportivo-pereira-vs-junior/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4060776/atletico-nacional-vs-la-equidad/ "
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046022/getafe-vs-elche/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046024/granada-vs-real-betis/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043916/aldosivi-vs-godoy-cruz/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043915/sarmiento-vs-huracan/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043914/independiente-vs-lanus/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043908/san-lorenzo-vs-racing-club/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043917/gimnasia-la-plata-vs-velez-sarsfield/ "
 ];
 
 let Enl = [
@@ -113,15 +93,13 @@ let Enl = [
   "",
   "",
   "",
-  "",
 
   "",
   "",
   "",
   "",
-  "",
-  "",
 
+  "",
   "",
   "",
 
@@ -141,6 +119,6 @@ let Enl = [
   "error"
 ];
 
-let HOY = "12/09/2021";
+let HOY = "13/09/2021";
 
 //console.log<b>(Est)</b></b>;
