635eb59f7ce90d7a130a6408b920a83921141668
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-09-17 21:36:53 -0300

    0101010101

--- public/match.js
+++ public/match.js
@@ -1,9 +1,19 @@
-let Title = [" Premier League ", " LaLiga ", "ERROR"];
-let Match = [" Newcastle United - Leeds United ", " Celta de Vigo - Cádiz "];
+let Title = [
+  " Premier League ",
+  " LaLiga ",
+  " Liga BetPlay Colombia ",
+  "ERROR"
+];
+let Match = [
+  " Newcastle United - Leeds United ",
+  " Celta de Vigo - Cádiz ",
+  " Medellín - Once Caldas "
+];
 
 let Est = [
   "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4027063/newcastle-united-vs-leeds-united/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046035/celta-de-vigo-vs-cadiz/ "
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046035/celta-de-vigo-vs-cadiz/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4060788/medellin-vs-once-caldas/ "
 ];
 
 let Enl = [
