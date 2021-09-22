fa30032842d26909a454155f8a7b3fa2feceee3e
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-09-22 02:07:42 -0300

    010101

--- public/match.js
+++ public/match.js
@@ -2,24 +2,43 @@ let Title = [
   " LaLiga ",
   " LaLiga ",
   " LaLiga ",
+  " LaLiga ",
+  " Ligue 1",
+
+  " Serie A ",
+  " Serie A ",
+  " Serie A ",
   " Serie A ",
   " Conmebol Libertadores ",
+  " Conmebol Sudamericana ",
   "ERROR"
 ];
 let Match = [
-  " Getafe - Atlético Madrid ",
-  " Athletic Club - Rayo Vallecano ",
-  " Levante - Celta de Vigo ",
-  " Fiorentina - Internazionale ",
-  " Palmeiras - Atlético Mineiro "
+  " Espanyol - Deportivo Alavés ",
+  " Sevilla - Valencia ",
+  " Real Madrid - Mallorca ",
+  " Villarreal - Elche ",
+  " Metz - PSG ",
+  " Salernitana - Hellas Verona ",
+  " Spezia - Juventus ",
+  " Cagliari - Empoli ",
+  " Milan - Venezia ",
+  " Flamengo - Barcelona ",
+  " RB Bragantino - Libertad "
 ];
 
 let Est = [
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046042/getafe-vs-atletico-madrid/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046041/athletic-club-vs-rayo-vallecano/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046046/levante-vs-celta-de-vigo/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064574/fiorentina-vs-internazionale/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4118853/palmeiras-vs-atletico-mineiro/ "
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046045/espanyol-vs-deportivo-alaves/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046049/sevilla-vs-valencia/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046048/real-madrid-vs-mallorca/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046050/villarreal-vs-elche/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4042826/metz-vs-psg/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064577/salernitana-vs-hellas-verona/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064579/spezia-vs-juventus/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064573/cagliari-vs-empoli/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4064575/milan-vs-venezia/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4120446/flamengo-vs-barcelona/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4120444/rb-bragantino-vs-libertad/ "
 ];
 
 let Enl = [
@@ -28,21 +47,24 @@ let Enl = [
   "",
 
   "",
+  "",
+  "",
+  "",
+
+  "",
+  "",
 
   "",
 
   "",
   "",
+
   "",
   "",
   "",
   "",
   "",
-  "",
-  "",
-  "",
-  "",
-  "",
+
   "",
   "",
   "",
@@ -50,6 +72,6 @@ let Enl = [
   "error"
 ];
 
-let HOY = "21/09/2021";
+let HOY = "22/09/2021";
 
 //console.log<b>(Est)</b></b>;
