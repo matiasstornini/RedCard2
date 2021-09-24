d2c7c6f489034a1094cf0f47c7109dc37d0ec573
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-09-24 02:27:39 -0300

    101011

--- public/match.js
+++ public/match.js
@@ -1,23 +1,25 @@
 let Title = [
-  " LaLiga ",
-  " LaLiga ",
-  " LaLiga " /*" LaLiga ",*/,
-
-  " Conmebol Sudamericana ",
+  " Bundesliga ",
+  " Liga Profesional Argentina ",
+  " Liga Profesional Argentina ",
+  " Liga Dimayor ",
+  " Liga Dimayor ",
   "ERROR"
 ];
 let Match = [
-  " Granada - Real Sociedad ",
-  " Osasuna - Real Betis ",
-  " Cádiz - Barcelona ",
-  " Peñarol - Athletico-PR "
+  " Greuther Fürth - Bayern München ",
+  " Sarmiento - Vélez Sarsfield ",
+  " Estudiantes - Platense ",
+  " Deportivo Cali - Deportivo Pereira ",
+  " Once Caldas - Alianza Petrolera "
 ];
 
 let Est = [
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046044/granada-vs-real-sociedad/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046047/osasuna-vs-real-betis/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046043/cadiz-vs-barcelona/ ",
-  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4120442/penarol-vs-athletico-pr/ "
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4042508/greuther-f-rth-vs-bayern-m-nchen/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043942/sarmiento-vs-velez-sarsfield/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043932/estudiantes-vs-platense/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4060794/deportivo-cali-vs-deportivo-pereira/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4060791/once-caldas-vs-alianza-petrolera/ "
 ];
 
 let Enl = [
@@ -25,6 +27,7 @@ let Enl = [
   "",
   "",
 
+  "",
   "",
   "",
   "",
@@ -50,6 +53,6 @@ let Enl = [
   "error"
 ];
 
-let HOY = "23/09/2021";
+let HOY = "24/09/2021";
 
 //console.log<b>(Est)</b></b>;
