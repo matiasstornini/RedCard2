2059ff93942fe67e9f045d04c64f19c0526935e4
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-10-01 02:15:07 -0300

    10011

--- public/match.js
+++ public/match.js
@@ -1,47 +1,33 @@
 let Title = [
-  "Europa League",
-  "Europa League",
-  "Europa League",
-  "Europa League",
-  "Europa League",
-  "Europa League",
-  "Conmebol Sudamericana",
-  "Liga Dimayor",
-
+  " LaLiga ",
+  " Liga Profesional Argentina ",
+  " Liga Profesional Argentina ",
+  " Liga Dimayor Colombia ",
+  " Liga Dimayor Colombia ",
   "ERROR"
 ];
 let Match = [
-  " Legia Warszawa - Leicester City ",
-  " Napoli - Spartak Moskva ",
-  " Olympique Lyonnais - Brøndby ",
-  " Real Sociedad - Mónaco ",
-  " Olympique Marseille - Galatasaray ",
-  " Ferencváros - Real Betis ",
-  " Athletico-PR - Peñarol ",
-  " Deportivo Pasto - Once Caldas "
+  " Athletic Club - Deportivo Alavés ",
+  " Aldosivi - Unión Santa Fe ",
+  " Lanús - Central Córdoba SdE ",
+  " Deportes Tolima - Santa Fe ",
+  " Junior - Patriotas Boyacá "
 ];
 
 let Est = [
-  "https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4128761/legia-warszawa-vs-leicester-city/ ",
-  "https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4128762/napoli-vs-spartak-moskva/ ",
-  "https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4128758/olympique-lyonnais-vs-brondby/ ",
-  "https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4128759/real-sociedad-vs-monaco/ ",
-  "https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4128770/ferencvaros-vs-real-betis/ ",
-  "https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4120443/athletico-pr-vs-penarol/ ",
-  "https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4150506/deportivo-pasto-vs-once-caldas/ "
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4046061/athletic-club-vs-deportivo-alaves/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043944/aldosivi-vs-union-santa-fe/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4043949/lanus-vs-central-cordoba-sde/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4060809/deportes-tolima-vs-santa-fe/ ",
+  "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4060806/junior-vs-patriotas-boyaca/ "
 ];
 
 let Enl = [
   "",
-  "",
-  "",
-  "",
+
   "",
   "",
 
-  "",
-
-  "",
   "",
   "",
 
@@ -52,6 +38,6 @@ let Enl = [
   "error1"
 ];
 
-let HOY = "30/09/2021";
+let HOY = "01/10/2021";
 
 //console.log<b>(Est)</b></b>;
