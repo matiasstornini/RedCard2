52506de6189918921ce09c93bff51702252805ed
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-10-04 02:52:23 -0300

    11100110

--- public/match.js
+++ public/match.js
@@ -1,57 +1,10 @@
 let Title = [
-  " Premier League ",
-  " Premier League ",
-  " Premier League ",
-  " Premier League ",
-
-  " LaLiga ",
-  " LaLiga ",
-  " LaLiga ",
-  " LaLiga ",
-  " LaLiga ",
-
-  " Ligue 1 ",
-
-  " Serie A ",
-  " Serie A ",
-  " Serie A ",
-
-  " Bundesliga ",
-
   " Liga Profesional Argentina ",
   " Liga Profesional Argentina ",
-  " Liga Profesional Argentina ",
-  " Liga Profesional Argentina ",
-
-  " Liga Dimayor Colombia ",
-  " Liga Dimayor Colombia ",
-  " Liga Dimayor Colombia ",
 
   "ERROR"
 ];
-let Match = [
-  "Crystal Palace vs Leicester City",
-  "Tottenham Hotspur vs Aston Villa",
-  "West Ham United vs Brentford",
-  "Liverpool vs Manchester City",
-  "Elche vs Celta de Vigo",
-  "Espanyol vs Real Madrid",
-  "Getafe vs Real Sociedad",
-  "Villarreal vs Real Betis",
-  "Granada vs Sevilla",
-  "Rennes vs PSG",
-  "Fiorentina vs Napoli",
-  "Roma vs Empoli",
-  "Atalanta vs Milan",
-  "Bayern München vs Eintracht Frankfurt",
-  "Gimnasia La Plata vs Sarmiento",
-  "Rosario Central vs Argentinos Juniors",
-  "River Plate vs Boca Juniors",
-  "Racing Club vs Estudiantes",
-  "Millonarios vs Rionegro Águilas",
-  "Medellín vs Atlético Bucaramanga",
-  "Alianza Petrolera vs Jaguares de Córdoba"
-];
+let Match = ["Defensa y Justicia vs Talleres Córdoba", "Colón vs Banfield"];
 
 let Est = [
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
@@ -104,8 +57,6 @@ let Enl = [
   "",
 
   "",
-  "",
-  ACol[0],
 
   "error1",
   "error1",
@@ -114,6 +65,6 @@ let Enl = [
   "error1"
 ];
 
-let HOY = "03/10/2021";
+let HOY = "04/10/2021";
 
 //console.log<b>(Est)</b></b>;
