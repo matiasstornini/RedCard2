a27e80305c5f726813bf7373b0b8fa3853325588
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-10-07 00:59:20 -0300

    0101

--- public/match.js
+++ public/match.js
@@ -1,8 +1,28 @@
-let Title = ["UEFA Nations League", "ERROR"];
-let Match = ["Italia vs España"];
+let Title = [
+  " UEFA Nations League ",
+  " Eliminatorias Conmebol ",
+  " Eliminatorias Conmebol ",
+  " Eliminatorias Conmebol ",
+  " Eliminatorias Conmebol ",
+  " Eliminatorias Conmebol ",
+  "ERROR"
+];
+let Match = [
+  "Bélgica - Francia",
+  "Uruguay - Colombia",
+  "Paraguay - Argentina",
+  "Venezuela - Brazil",
+  "Ecuador - Bolivia",
+  "Peru - Chile"
+];
 
 let Est = [
-  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://betsapi.com/r/2998730/Italy-v-Spain ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/2998706/Belgium-v-France ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3508352/Uruguay-v-Colombia ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3508382/Paraguay-v-Argentina ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3508383/Venezuela-v-Brazil ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3508364/Ecuador-v-Bolivia ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3508402/Peru-v-Chile ",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
@@ -29,27 +49,11 @@ let Est = [
 
 let Enl = [
   "",
-  "",
 
   "",
   "",
   "",
   "",
-  "",
-
-  "",
-
-  "",
-  "",
-  "",
-
-  "",
-
-  "",
-  "",
-  "",
-  "",
-
   "",
 
   "error1",
@@ -59,6 +63,6 @@ let Enl = [
   "error1"
 ];
 
-let HOY = "06/10/2021";
+let HOY = "07/10/2021";
 
 //console.log<b>(Est)</b></b>;
