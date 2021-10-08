05f014b0cc55d04416ec6897185b12afd4bad8cf
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-10-08 02:36:52 -0300

    010101

--- public/match.js
+++ public/match.js
@@ -1,28 +1,38 @@
 let Title = [
-  " UEFA Nations League ",
-  " Eliminatorias Conmebol ",
-  " Eliminatorias Conmebol ",
-  " Eliminatorias Conmebol ",
-  " Eliminatorias Conmebol ",
-  " Eliminatorias Conmebol ",
+  " Eliminatorias UEFA ",
+  " Liga Profesional Argentina ",
+  " Liga Profesional Argentina ",
+  " Liga Profesional Argentina ",
+  " Liga Profesional Argentina ",
+  " Liga Profesional Argentina ",
+  " Liga Dimayor Colombia ",
+  " Liga Dimayor Colombia ",
+  " Liga Dimayor Colombia ",
+  " Liga Dimayor Colombia ",
   "ERROR"
 ];
 let Match = [
-  "Bélgica - Francia",
-  "Uruguay - Colombia",
-  "Paraguay - Argentina",
-  "Venezuela - Brazil",
-  "Ecuador - Bolivia",
-  "Peru - Chile"
+  "Alemania - Rumania",
+  "Unión Santa Fe - Platense",
+  "Central Córdoba SdE - Huracán",
+  "Estudiantes - Rosario Central",
+  "Newell's Old Boys - Vélez Sarsfield",
+  "Jaguares de Córdoba - Deportivo Pasto",
+  "Rionegro Águilas - Alianza Petrolera",
+  "Medellín - Envigado"
 ];
 
 let Est = [
-  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/2998706/Belgium-v-France ",
-  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3508352/Uruguay-v-Colombia ",
-  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3508382/Paraguay-v-Argentina ",
-  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3508383/Venezuela-v-Brazil ",
-  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3508364/Ecuador-v-Bolivia ",
-  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3508402/Peru-v-Chile ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3018643/Germany-v-Romania ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3710548/Union-Santa-Fe-v-CA-Platense ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3710543/Central-Cordoba-SdE-v-Huracan ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3710266/Estudiantes-LP-v-Rosario-Central ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3710130/Newell%27s-v-Velez-Sarsfield ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3797184/Jaguares-de-Cordoba-v-Deportivo-Pasto ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3797186/Aguilas-Doradas-v-Alianza-Petrolera ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3797262/Independiente-Medellin-v-Envigado-FC ",
+
+  /**/
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
@@ -77,6 +87,6 @@ let Enl = [
   "error1"
 ];
 
-let HOY = "07/10/2021";
+let HOY = "08/10/2021";
 
 //console.log<b>(Est)</b></b>;
