7d9feec9350c3127bf69ef9f6d0cbbb05706f388
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-10-20 01:35:04 -0300

    100101

--- public/match.js
+++ public/match.js
@@ -11,6 +11,7 @@ let Title = [
   " Liga Profesional Argentina ",
   " Liga Profesional Argentina ",
   " Liga Profesional Argentina ",
+  " Liga Profesional Argentina ",
 
   /**/
   "ERROR",
@@ -22,30 +23,22 @@ let Title = [
   "ERROR"
 ];
 let Match = [
-  "Beşiktaş - Sporting CP",
-  "Club Brugge - Manchester City",
-  "Ajax - Borussia Dortmund",
-  "Atlético Madrid - Liverpool",
-  "Internazionale - Sheriff",
-  "Porto - Milan",
-  "PSG - RB Leipzig",
-  "Shakhtar Donetsk - Real Madrid",
-  "Platense - Rosario Central",
-  "Unión Santa Fe - Racing Club",
-  "Patronato - Defensa y Justicia",
-  "Central Córdoba SdE - Vélez Sarsfield"
+  "Barcelona - Dynamo Kyiv",
+  "Salzburg - Wolfsburg",
+  "Benfica - Bayern München",
+  "Chelsea - Malmö FF",
+  "Lille - Sevilla",
+  "Manchester United - Atalanta",
+  "Young Boys - Villarreal",
+  "Zenit - Juventus",
+  "Arsenal - Gimnasia La Plata",
+  "San Lorenzo - Lanús",
+  "Banfield - Huracán",
+  "Estudiantes - Atlético Tucumán",
+  "Boca Juniors - Godoy Cruz"
 ];
 
 let Est = [
-  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3983269/Besiktas-v-Sporting ",
-  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3982932/Club-Brugge-v-Man-City ",
-  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3982759/Ajax-v-Borussia-Dortmund ",
-  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3982937/Atletico-Madrid-v-Liverpool ",
-  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3982765/Inter-Milan-v-Sheriff-Tiraspol ",
-  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3983265/FC-Porto-v-AC-Milan ",
-  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3982756/PSG-v-RB-Leipzig ",
-  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3983272/Shakhtar-Donetsk-v-Real-Madrid ",
-
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
@@ -86,7 +79,8 @@ let Enl = [
   "",
   "",
   "",
-  AArg[0],
+  "",
+  "",
 
   "error1",
   "error1",
