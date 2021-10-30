54923beb637fb51caed7d3fa53f115b463b8d2ef
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-10-30 16:14:37 -0300

    0010101

--- src/titulo.js
+++ src/titulo.js
@@ -4,11 +4,15 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import InstagramIcon from "@material-ui/icons/Instagram";
+import Typography from "@material-ui/core/Typography";
 
 //componente a exportar
 const Titulo = () => {
   return (
     <div>
+      <Typography variant="h6" component="h6">
+        SEGUINOS!
+      </Typography>
       <Button
         color="primary"
         target="_blank"
