e92cc0439ed584c31551c9755e4f86f6396a8f4e
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-11-21 16:37:04 -0300

    010101011111

--- public/match.js
+++ public/match.js
@@ -62,7 +62,13 @@ let Est = [
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
-  "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
+  /* */
+
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3797289/Alianza-Petrolera-v-Junior ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3797210/Atletico-Nacional-Medellin-v-Millonarios ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3797290/Deportivo-Cali-v-Once-Caldas ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3797211/Deportivo-Pasto-v-Independiente-Medellin ",
+  "https://pirlotv2.herokuapp.com/full/app/test/general-ext?url=https://es.betsapi.com//r/3797291/Deportivo-Pereira-v-America-de-Cali ",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
