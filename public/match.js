f2f5036c0f47167b38e0c07a1ec6e1a6a414eafe
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-12-04 00:43:27 -0300

    101101011

--- public/match.js
+++ public/match.js
@@ -91,11 +91,11 @@ let Est = [
 ];
 
 let Enl = [
-  "",
+  AIng[0],
   "",
   "",
 
-  "",
+  AEsp[0],
   "",
   "",
   "",
@@ -130,6 +130,6 @@ let Enl = [
   "error!!1"
 ];
 
-let HOY = "03/12/2021";
+let HOY = "04/12/2021";
 
 //console.log<b>(Est)</b></b>;
