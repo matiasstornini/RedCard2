8a68f570f933a2036af45fd72409c35670a31c6c
adiccfut <37271131+adiccfut@users.noreply.github.com>
2022-01-05 19:15:23 -0300

    110101010

--- src/App.js
+++ src/App.js
@@ -50,10 +50,10 @@ export default function ContainedButtons() {
   let BB = [
     "",
     "",
-    bloque.Extra[0],
+    "",
     bloque.AEsp[0],
 
-    bloque.AIng[0],
+    "",
 
     /** */
     "error!!1",
