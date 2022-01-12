d90972b9d67a90baf26b32c2eb95eb4690ce4d65
adiccfut <37271131+adiccfut@users.noreply.github.com>
2022-01-12 02:18:35 -0300

    010100101

--- public/match.js
+++ public/match.js
@@ -1,6 +1,7 @@
 let Title = [
-  "FA CUP",
-  "LaLiga",
+  "SUPERCOPA España",
+  "EFL CUP",
+  "SUPERCOPA Italia",
 
   /**/
   "ERROR",
@@ -12,11 +13,17 @@ let Title = [
   "ERROR",
   "ERROR"
 ];
-let Match = ["Manchester United - Aston Villa", "Espanyol - Elche"];
+let Match = [
+  "Barcelona - Real Madrid",
+  "Tottenham Hotspur - Chelsea",
+
+  "Internazionale - Juventus"
+];
 
 let Est = [
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,manchester-united,aston-villa,545571.php",
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,espanyol-barcelona,elche,509813.php",
+  "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
+  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,tottenham-hotspur,chelsea-london,546017.php",
+  "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
 
   /** ***TEST*** */
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
