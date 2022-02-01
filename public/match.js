b82a0b97f13829b4a064e5ba8be48e7e261cbe40
adiccfut <37271131+adiccfut@users.noreply.github.com>
2022-02-01 16:43:40 -0300

    001011010

--- public/match.js
+++ public/match.js
@@ -24,6 +24,11 @@ let Match = [
 ];
 
 let Est = [
+  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,bolivia,chile,483713.php",
+  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,uruguay,venezuela,483716.php",
+  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,argentina,colombia,483717.php",
+  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,brasil,paraguay,483715.php",
+  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,peru,ecuador,483714.php",
   /** ***TEST*** */
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
