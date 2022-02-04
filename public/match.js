3323bbb8110c191ce010730c58d11dcaaeeb078e
adiccfut <37271131+adiccfut@users.noreply.github.com>
2022-02-04 00:53:27 -0300

    010010110

--- public/match.js
+++ public/match.js
@@ -1,9 +1,7 @@
 let Title = [
-  "Copa del Rey",
-  "Copa del Rey",
+  "FA Cup",
+  "LaLiga",
 
-  "Liga Dimayor Colombia",
-  "Liga Dimayor Colombia",
   "Liga Dimayor Colombia",
   "Liga Dimayor Colombia",
 
@@ -18,23 +16,13 @@ let Title = [
   "ERROR"
 ];
 let Match = [
-  "Real Sociedad - Real Betis",
-  "Athletic Club - Real Madrid",
-
-  "La Equidad - Rionegro Águilas",
-  "Unión Magdalena - Deportivo Pasto",
-  "Jaguares de Córdoba - Patriotas Boyacá",
-  "Atlético Nacional - Deportes Tolima"
+  "Manchester United - Middlesbrough",
+  "Getafe - Levante",
+  "Cortuluá - América de Cali",
+  "Once Caldas - Medellín"
 ];
 
 let Est = [
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,real-sociedad,real-betis,547874.php",
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,athletic-bilbao,real-madrid,547875.php",
-
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,la-equidad,rionegro-aguilas,547545.php",
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,union-magdalena,deportivo-pasto,547546.php",
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,jaguares-de-cordoba,patriotas,547547.php",
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,atletico-nacional,deportes-tolima,547548.php",
   /** ***TEST*** */
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
