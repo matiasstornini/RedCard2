5c6cb5549214fdcd68a4aa16913799a3b4f29b00
adiccfut <37271131+adiccfut@users.noreply.github.com>
2022-02-21 03:06:09 -0300

    00001010

--- public/match.js
+++ public/match.js
@@ -1,23 +1,11 @@
 let Title = [
   "LaLiga",
-  "LaLiga",
-  "LaLiga",
-  "LaLiga",
-
-  "Premier League",
-
-  "Bundesliga",
-  "Bundesliga",
-
   "Serie A",
 
   "Liga Profesional Argentina",
   "Liga Profesional Argentina",
   "Liga Profesional Argentina",
-
-  "Liga Dimayor Colombia",
-  "Liga Dimayor Colombia",
-  "Liga Dimayor Colombia",
+  "Liga Profesional Argentina",
 
   /**/
   "ERROR",
@@ -30,48 +18,17 @@ let Title = [
   "ERROR"
 ];
 let Match = [
-  "Espanyol - Sevilla",
-  "Valencia - Barcelona",
-  "Real Betis - Mallorca",
-  "Athletic Club - Real Sociedad",
+  "Celta de Vigo - Levante",
 
-  "Leeds United - Manchester United",
+  "Cagliari - Napoli",
 
-  "Bayern München - Greuther Fürth",
-  "Borussia Dortmund - Borussia M'gladbach",
-
-  "Internazionale - Sassuolo",
-
-  "Platense - Sarmiento",
-  "Boca Juniors - Rosario Central",
-  "Newell's Old Boys - River Plate",
-
-  "Cortuluá - Deportivo Pereira",
-  "Deportivo Cali - Alianza Petrolera",
-  "Junior - América de Cali"
+  "Barracas Central - Tigre",
+  "Banfield - Gimnasia La Plata",
+  "Godoy Cruz - Aldosivi",
+  "San Lorenzo - Defensa y Justicia"
 ];
 
 let Est = [
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,espanyol-barcelona,sevilla,509867.php",
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,valencia,fc-barcelona,509871.php",
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,real-betis,mallorca,509863.php",
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,athletic-bilbao,real-sociedad,509862.php",
-
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,leeds-united,manchester-united,505898.php",
-
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,bayern-munchen,greuther-furth,508830.php",
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,borussia-dortmund,borussia-mgladbach,508831.php",
-
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,internazionale-milano,sassuolo,523535.php",
-
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,platense,sarmiento,549185.php",
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,boca-juniors,rosario-central,549186.php",
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,newells-old-boys,river-plate,549187.php",
-
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,cortulua,deportivo-pereira,547584.php",
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,deportivo-cali,alianza-petrolera,547585.php",
-  "https://pirlotv2.herokuapp.com/full/app/test/est?url=https://es.fcstats.com/partido,atletico-junior,america-de-cali,547587.php",
-
   /** ***TEST*** */
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
