310c6b53587df37cbe72b15143a3f33a4bbdbad3
adiccfut <37271131+adiccfut@users.noreply.github.com>
2022-02-28 01:42:16 -0300

    1001010101

--- public/match.js
+++ public/match.js
@@ -1,22 +1,9 @@
 let Title = [
   "LaLiga",
-  "LaLiga",
-  "LaLiga",
-  "LaLiga",
-
-  "EFL CUP",
-
-  "Bundesliga",
-
-  "Serie A",
 
   "Liga Profesional Argentina",
   "Liga Profesional Argentina",
-  "Liga Profesional Argentina",
-  "Liga Profesional Argentina",
 
-  "Liga Dimayor Colombia",
-  "Liga Dimayor Colombia",
   "Liga Dimayor Colombia",
 
   /**/
@@ -30,50 +17,17 @@ let Title = [
   "ERROR"
 ];
 let Match = [
-  "Villarreal - Espanyol",
-  "Sevilla - Real Betis",
-  "Real Sociedad - Osasuna",
-  "Barcelona - Athletic Club",
+  "Granada - Cádiz",
 
-  "Chelsea - Liverpool",
+  "Talleres Córdoba - Newell's Old Boys",
+  "Lanús - Tigre",
 
-  "Augsburg - Borussia Dortmund",
-
-  "Lazio - Napoli",
-
-  "Estudiantes - Arsenal",
-  "Sarmiento - Unión Santa Fe",
-  "River Plate - Racing Club",
-  "Argentinos Juniors - San Lorenzo",
-
-  "Unión Magdalena - Deportivo Cali",
-  "Atlético Bucaramanga - Atlético Nacional",
-  "América de Cali - Once Caldas"
+  "Patriotas Boyacá - Santa Fe"
 ];
 
 var Est = [];
 
 let ustat = [
-  "https://es.fcstats.com/partido,villarreal,espanyol-barcelona,509881.php",
-  "https://es.fcstats.com/partido,sevilla,real-betis,509880.php",
-  "https://es.fcstats.com/partido,real-sociedad,osasuna,509879.php",
-  "https://es.fcstats.com/partido,fc-barcelona,athletic-bilbao,509873.php",
-
-  "https://es.fcstats.com/partido,chelsea-london,liverpool,547936.php",
-
-  "https://es.fcstats.com/partido,augsburg,borussia-dortmund,508837.php",
-
-  "https://es.fcstats.com/partido,ss-lazio,napoli,523545.php",
-
-  "https://es.fcstats.com/partido,estudiantes-de-la-plata,arsenal-de-sarandi,549201.php",
-  "https://es.fcstats.com/partido,sarmiento,union-de-santa-fe,549208.php",
-  "https://es.fcstats.com/partido,river-plate,racing-club,549206.php",
-  "https://es.fcstats.com/partido,argentinos-juniors,san-lorenzo,549197.php",
-
-  "https://es.fcstats.com/partido,union-magdalena,deportivo-cali,547600.php",
-  "https://es.fcstats.com/partido,atletico-bucaramanga,atletico-nacional,547592.php",
-  "https://es.fcstats.com/partido,america-de-cali,once-caldas,547591.php",
-
   /** ***TEST*** */
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
@@ -102,28 +56,15 @@ let ustat = [
 ];
 
 for (var i = 0; i <= ustat.length - 23; i++) {
-  Est.push("https://pirlotv2.herokuapp.com/full/app/test/est?url=" + ustat[i]);
+  Est.push("" + ustat[i]);
 }
 
 let Enl = [
   "",
-  "",
-  "",
-  "",
-
-  "",
-
-  "",
-
-  "",
 
   "",
   "",
-  "",
-  "",
 
-  "",
-  "",
   "",
 
   /** */
