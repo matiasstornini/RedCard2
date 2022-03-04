acc1618056c78d77737e0190fda810d5bd5b8ef0
adiccfut <37271131+adiccfut@users.noreply.github.com>
2022-03-04 19:02:28 -0300

    0110100

--- public/match.js
+++ public/match.js
@@ -62,11 +62,11 @@ for (var i = 0; i <= ustat.length - 23; i++) {
 }
 
 let Enl = [
-  AEsp[0],
-
-  AIta[0],
+  "",
 
   "",
+
+  Extra[0],
   "",
   "",
 
--- src/App.js
+++ src/App.js
@@ -48,11 +48,11 @@ export default function ContainedButtons() {
   var lis = [];
 
   let BB = [
-    bloque.AEsp[0],
-
-    bloque.AIta[0],
+    "",
 
     "",
+
+    bloque.Extra[0],
     "",
     "",
 
