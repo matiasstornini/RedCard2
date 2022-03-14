8a49945af920fa8d556f445f717e726611fc4a6f
adiccfut <37271131+adiccfut@users.noreply.github.com>
2022-03-14 03:35:00 -0300

    01010101

--- public/match.js
+++ public/match.js
@@ -1,21 +1,8 @@
 let Title = [
-  "Ligue 1",
+  "LaLiga",
 
   "Premier League",
-  "Premier League",
 
-  "LaLiga",
-  "LaLiga",
-  "LaLiga",
-  "LaLiga",
-
-  "Serie A",
-  "Serie A",
-
-  "Bundesliga",
-
-  "Liga Profesional Argentina",
-  "Liga Profesional Argentina",
   "Liga Profesional Argentina",
   "Liga Profesional Argentina",
   "Liga Profesional Argentina",
@@ -31,50 +18,18 @@ let Title = [
   "ERROR"
 ];
 let Match = [
-  "PSG - Bordeaux",
+  "Mallorca - Real Madrid",
 
-  "Chelsea - Newcastle United",
-  "Arsenal - Leicester City",
+  "Crystal Palace - Manchester City",
 
-  "Rayo Vallecano - Sevilla",
-  "Real Betis - Athletic Club",
-  "Real Sociedad - Deportivo Alavés",
-  "Barcelona - Osasuna",
-
-  "Hellas Verona - Napoli",
-  "Torino - Internazionale",
-
-  "Borussia Dortmund - Arminia Bielefeld",
-
-  "Lanús - Colón",
-  "Unión Santa Fe - Banfield",
-  "River Plate - Gimnasia La Plata",
-  "Estudiantes - Boca Juniors"
+  "Atlético Tucumán - Racing Club",
+  "Argentinos Juniors - Defensa y Justicia",
+  "Independiente - Central Córdoba SdE"
 ];
 
 var Est = [];
 
 let ustat = [
-  "https://es.fcstats.com/partido,paris-saint-germain,bordeaux,508147.php",
-
-  "https://es.fcstats.com/partido,chelsea-london,newcastle-united,505927.php",
-  "https://es.fcstats.com/partido,arsenal-london,leicester-city,505924.php",
-
-  "https://es.fcstats.com/partido,rayo-vallecano,sevilla,509899.php",
-  "https://es.fcstats.com/partido,real-betis,athletic-bilbao,509894.php",
-  "https://es.fcstats.com/partido,real-sociedad,deportivo-alaves,509900.php",
-  "https://es.fcstats.com/partido,fc-barcelona,osasuna,509893.php",
-
-  "https://es.fcstats.com/partido,hellas-verona,napoli,523570.php",
-  "https://es.fcstats.com/partido,torino,internazionale-milano,523568.php",
-
-  "https://es.fcstats.com/partido,borussia-dortmund,arminia-bielefeld,508858.php",
-
-  "https://es.fcstats.com/partido,lanus,colon,549231.php",
-  "https://es.fcstats.com/partido,union-de-santa-fe,banfield,549237.php",
-  "https://es.fcstats.com/partido,river-plate,gimnasia-la-plata,549233.php",
-  "https://es.fcstats.com/partido,estudiantes-de-la-plata,boca-juniors,549228.php",
-
   /** ***TEST*** */
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
@@ -102,9 +57,9 @@ let ustat = [
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estaisticas-no-disp.html"
 ];
 
-for (var i = 0; i <= ustat.length - 23; i++) {
-  //Est.push("" + ustat[i]);
-  Est.push("https://pirlotv2.herokuapp.com/full/app/test/est?url=" + ustat[i]);
+for (var i = 0; i <= ustat.length - 0; i++) {
+  Est.push("" + ustat[i]);
+  //Est.push("https://pirlotv2.herokuapp.com/full/app/test/est?url=" + ustat[i]);
 }
 
 let Enl = [
